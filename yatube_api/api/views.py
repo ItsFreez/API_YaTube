a77a851b8cc85544f7a263a66525b82cd07cf8dc
Игорь Митяшин <ItsFreez@yandex.ru>
2023-09-12 10:20:03 +0300

    Add all models for work

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,39 @@
-# TODO:  Напишите свой вариант
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets, permissions
+from rest_framework.pagination import LimitOffsetPagination
+
+from api.permissions import IsOwnerOrReadOnly
+from api.serializers import CommentSerializer, GroupSerializer, PostSerializer
+from posts.models import Comment, Group, Post
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = (IsOwnerOrReadOnly, permissions.IsAuthenticated)
+
+    def get_post_object(self):
+        return get_object_or_404(Post, id=self.kwargs['post_id'])
+
+    def get_queryset(self):
+        return Comment.objects.filter(post=self.get_post_object())
+
+    def perform_create(self, serializer):
+        serializer.save(
+            author=self.request.user,
+            post=self.get_post_object()
+        )
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (IsOwnerOrReadOnly, permissions.IsAuthenticated)
+    pagination_class = LimitOffsetPagination
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
